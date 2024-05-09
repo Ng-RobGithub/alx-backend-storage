67b5bf51e03b639f92a9da8bc16347c052789f0e
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-09 22:26:36 +0100

    My backend Advanced Score2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
---This SQL script ranks the country origins of bands based on the number of fans.
+--This SQL script ranks the country origins of bands based on the 
+-- number of fans.
 SELECT origin, COUNT(fan_id) AS nb_fans
 FROM bands
 GROUP BY origin
