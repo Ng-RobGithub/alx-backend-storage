d3e211f8bb53be9eaf4ef8ac8902bba92acf16c8
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-09 23:03:32 +0100

    My backend Advanced Score3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,8 @@
--- This SQL script lists all bands with Glam rock as their main style, ranked by their longevity.
+-- Lists all bands with Glam rock as their style,
+-- ranked by their longevity
+-- Column names must be: band_name & lifespan
 
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+ORDER BY lifespan DESC;
