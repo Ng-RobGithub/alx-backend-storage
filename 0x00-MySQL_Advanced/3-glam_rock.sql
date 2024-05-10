c93cfd0a966a6330365d411089b9cf340b9a9c52
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-10 23:35:39 +0100

    Update Advanced mysqldb

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,7 @@
--- Select bands with Glam rock as their main style and rank them by longevity
-SELECT 
-    band_name, 
-    (SUBSTRING_INDEX(lifespan, '-', 1) + 2022) - SUBSTRING_INDEX(lifespan, '-', -1) AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    main_style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+-- Select bands with Glam rock as their main style and calculate lifespan
+SELECT band_name, 
+    (IF(split(lifespan, '–')[2] = 'present', 2022 - CAST(split(lifespan, '–')[1] AS UNSIGNED), 
+    2022 - CAST(split(lifespan, '–')[2] AS UNSIGNED))) AS lifespan
+FROM metal_bands
+WHERE formed LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
