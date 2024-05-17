088c8646cdd76f8741514221921378cdf841756e
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-17 21:30:42 +0100

    Update redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,9 +6,6 @@ import requests
 from functools import wraps
 from typing import Callable
 
-''' Initialize the Redis client '''
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
-
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
@@ -38,18 +35,3 @@ def get_page(url: str) -> str:
     and tracking the request.
     '''
     return requests.get(url).text
-
-
-def test_redis():
-    redis_client.set('test', 'value')
-    value = redis_client.get('test')
-    print(value.decode('utf-8'))
-
-# Example usage
-    test_url = ("http://slowwly.robertomurray.co.uk/delay/5000/"
-                "url/http://www.example.com")
-    print(get_page(test_url))
-    print(get_page(test_url))  # This should be a cache hit
-    # To see the count
-    count = redis_client.get(f"count:{test_url}").decode('utf-8')
-    print(f"Access count for {test_url}: {count}")
