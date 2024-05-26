d486214f60458ec35e04216afcd2f9d8853e296c
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-26 21:07:40 +0100

    Create SafeDiv to div two numbers

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,13 +1,15 @@
--- Description: This SQL script creates a function called SafeDiv that divides two numbers and returns the result or 0 if the second number is equal to 0.
+-- Description: This SQL script creates a function called SafeDiv that divides two numbers 
+-- and returns the result or 0 if the second number is equal to 0.
 -- The function takes two integer arguments: a and b.
 -- It returns a DECIMAL(10,4) value representing the division of a by b, or 0 if b is equal to 0.
--- Author: [Your Name]
--- Date: [Current Date]
+-- Author: [Ngozi Rob Agomuonso]
+-- Date: [26/05/2024]
 -- Version: 1.0
 
 DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNNCTION SafeDiv(a INT, b INT)
 RETURNS DECIMAL(10,4)
 BEGIN
     IF b = 0 THEN
