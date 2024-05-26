36903ddc6bfee4534ed665ae7d857a2bdb1b02bc
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-26 13:17:55 +0100

    reset emails

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,15 +1,17 @@
---This SQL script creates a trigger that resets the attribute valid_email only when the email has been changed.
+--This SQL script creates a trigger that resets the attribute valid_email
+--only when the email has been changed.
+--@author: Ngozi Rob Agomuonso <https://github.com/Ng-RobGithub>
 
-
-DELIMITER //
-
-CREATE TRIGGER reset_valid_email AFTER UPDATE ON users
+DROP TRIGGER IF EXISTS validate_email;
+DELIMITER $$
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email THEN
         SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
     END IF;
-END;
-//
-
+END $$
 DELIMITER ;
