48b916fd006c87889f30a0968eaac14238cb0559
Ng-RobGithub <ngrob4real@gmail.com>
2024-05-26 21:24:17 +0100

    List all students wit less than 80 score

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,7 @@
--- Create a view named 'need_meeting' that lists all students who have a score under 80 (strict) and no 'last_meeting' or more than one month.
+-- Create a view named 'need_meeting' that lists all students who have a score under 80 (strict)
+-- and no 'last_meeting' or more than one month.
+
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
